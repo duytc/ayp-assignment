51b9be735e5612452d851055e47e6f6bb94ffbb6
Muhammad Alfian Rasyidin <alfian.rasyidin@ayp-group.com>
2022-03-28 11:26:16 +0700

    Add example endpoint

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function checkDbConnection(): JsonResponse
+    {
+        return response()->json([
+            'data' => [
+                'dbConnection' => !!DB::getPdo(),
+            ]
+        ]);
+    }
 }
