37eb140cae9a872eac7b0635f1db2ffdc99a4f23
john <tranduy8683@gmail.com>
2022-06-20 17:38:08 -0700

    create workers and employments table

---
+++ database/migrations/2022_06_19_202300_create_employment_table_20220619.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    protected  $table ='employments';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('worker_id');
+            $table->string('company_name');
+            $table->string('job_title');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->table);
+    }
+};
